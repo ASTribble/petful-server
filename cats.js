9a50559b0b803197aa50420e79a8184df06ddf62
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-09 19:54:26 -0800

    updated cat descriptions and doubled the seed array

--- cats.js
+++ cats.js
@@ -24,21 +24,57 @@ const catArray = [
   },
   {
     imageURL: 'static/pics/small_cats/smokey.jpeg',
-    imageDescription: 'Wide-eyed charmer',
-    name: 'Fluffy',
+    imageDescription: 'Wide-eyed grey and white cat gazing into the camera.',
+    name: 'Smokey',
     sex: 'Female',
-    age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
+    age: 1,
+    breed: 'Short-hair',
+    story: 'Only wants to be adored.'
   },
   {
     imageURL: 'static/pics/small_cats/stretch.jpeg',
-    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
-    name: 'Fluffy',
-    sex: 'Female',
+    imageDescription: 'Adorable cat stretching-out',
+    name: 'Stretch',
+    sex: 'Male',
     age: 2,
-    breed: 'Bengal',
-    story: 'Thrown on the street'
+    breed: 'Short-hair',
+    story: 'Wants belly rubs and a forever home.'
+  },
+  {
+    imageURL: 'static/pics/small_cats/alice.jpeg',
+    imageDescription: 'Kitten with grey head and white face and chin.',
+    name: 'Alice',
+    sex: 'Female',
+    age: .5,
+    breed: 'Calico',
+    story: 'Likes scary movies.  Got lost in the couch.'
+  },
+  {
+    imageURL: 'static/pics/small_cats/harry.jpeg',
+    imageDescription: 'Small grey smoosh-faced cat.',
+    name: 'Harry',
+    sex: 'Male',
+    age: 1,
+    breed: 'Blue',
+    story: 'Ran into a wall and got disoriented.'
+  },
+  {
+    imageURL: 'static/pics/small_cats/smokey.jpeg',
+    imageDescription: 'Wide-eyed grey and white cat gazing into the camera.',
+    name: 'Smokey',
+    sex: 'Female',
+    age: 1,
+    breed: 'Short-hair',
+    story: 'Only wants to be adored.'
+  },
+  {
+    imageURL: 'static/pics/small_cats/stretch.jpeg',
+    imageDescription: 'Adorable cat stretching-out',
+    name: 'Stretch',
+    sex: 'Male',
+    age: 2,
+    breed: 'Short-hair',
+    story: 'Wants belly rubs and a forever home.'
   }
 ];
 
