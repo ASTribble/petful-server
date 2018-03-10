74af4bd87cfea3d7f9b8e47c011fb2380c4175b2
A. Scott Tribble <a.scott.tribble@gmail.com>
2018-03-09 20:00:36 -0800

    updated dog data and put them into the seed data twice

--- dogs.js
+++ dogs.js
@@ -8,9 +8,9 @@ const dogArray = [
     imageDescription: 'White and grey dog pondering her existence.',
     name: 'Grover',
     sex: 'Female',
-    age: 3,
-    breed: 'Mutt',
-    story: 'In existential crisis'
+    age: 4,
+    breed: 'Short-haired mutt',
+    story: 'In existential crisis.  Needs comfort.'
   },
   {
     imageURL: 'static/pics/small_dogs/larry.jpeg',
@@ -18,17 +18,17 @@ const dogArray = [
     name: 'Larry',
     sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
+    breed: 'Long-haired mutt.',
+    story: 'Wants to be your new best friend.'
   },
   {
     imageURL: 'static/pics/small_dogs/sparky.jpeg',
     imageDescription: 'Excited young bulldog peeking over a deck.',
     name: 'Sparky',
     sex: 'Male',
-    age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
+    age: 1,
+    breed: 'Bulldog',
+    story: 'Inquisitive and eager to please.'
   },
   {
     imageURL: 'static/pics/small_dogs/teacup',
@@ -36,8 +36,44 @@ const dogArray = [
     name: 'Teacup',
     sex: 'Male',
     age: 3,
-    breed: 'Golden Retriever',
-    story: 'Owner Passed away'
+    breed: 'Short-haired & cute',
+    story: 'Would go well with crumpets!'
+  },
+  {
+    imageURL: 'static/pics/small_dogs/grover.jpeg',
+    imageDescription: 'White and grey dog pondering her existence.',
+    name: 'Grover',
+    sex: 'Female',
+    age: 4,
+    breed: 'Short-haired mutt',
+    story: 'In existential crisis.  Needs comfort.'
+  },
+  {
+    imageURL: 'static/pics/small_dogs/larry.jpeg',
+    imageDescription: 'White and black dog with a toothy grin.',
+    name: 'Larry',
+    sex: 'Male',
+    age: 3,
+    breed: 'Long-haired mutt.',
+    story: 'Wants to be your new best friend.'
+  },
+  {
+    imageURL: 'static/pics/small_dogs/sparky.jpeg',
+    imageDescription: 'Excited young bulldog peeking over a deck.',
+    name: 'Sparky',
+    sex: 'Male',
+    age: 1,
+    breed: 'Bulldog',
+    story: 'Inquisitive and eager to please.'
+  },
+  {
+    imageURL: 'static/pics/small_dogs/teacup',
+    imageDescription: 'A very small brown and tan dog in a polka-dot cup.',
+    name: 'Teacup',
+    sex: 'Male',
+    age: 3,
+    breed: 'Short-haired & cute',
+    story: 'Would go well with crumpets!'
   }
 ];
 
